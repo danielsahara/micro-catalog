a503eb2ea6c6fe7a66224099b0712fb7738f39bd
Daniel Sahara <d.otasahara@gmail.com>
2020-07-08 23:55:57 -0300

    Criando executador de comandos - parte 2

--- src/commands/fixtures.command.ts
+++ src/commands/fixtures.command.ts
@@ -1,3 +1,8 @@
 export class FixturesCommand {
     static command = 'fixtures';
+    static description = 'fixtures data in Elastic';
+
+    async run() {
+        console.log('fixture executing');
+    }
 }
--- src/console.ts
+++ src/console.ts
@@ -1,16 +1,38 @@
 import * as commands from './commands';
+import {default as chalk} from 'chalk';
 
 const command = process.argv[2] || null;
 
 if (!command) {
-
+    showAvaiableCommands();
 }
 
 // @ts-ignore
 const commandKey: string | undefined = Object.keys(commands).find(c => commands[c].command === command)
 
 if (!commandKey) {
-
+    showAvaiableCommands();
 }
 
-console.log(commandKey);
+// @ts-ignore
+const commandInstance = new commands[commandKey];
+
+commandInstance
+    .run()
+    .catch((error: any) => {
+        console.error(error)
+    });
+
+function showAvaiableCommands() {
+    console.log(chalk.green('Loopback Console'));
+    console.log("");
+    console.log(chalk.green('Avaiable Commands'));
+    console.log("");
+
+    for (const c of Object.keys(commands)){
+        // @ts-ignore
+        console.log(`- ${chalk.green(commands[c].command)} - ${commands[c].description}`)
+    }
+    console.log("");
+    process.exit();
+}
