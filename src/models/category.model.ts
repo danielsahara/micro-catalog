4435c0ca0ab8ea69e3d62d454dbd65661ba48461
Daniel Sahara <d.otasahara@gmail.com>
2020-07-13 23:38:17 -0300

    Declarando regras de validações básicas nos models

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -9,9 +9,6 @@ export class Category extends Entity {
         id: true,
         generated: false,
         required: true,
-        jsonSchema: {
-            exists: ['Category', 'id']
-        }
     })
     id: string;
 
